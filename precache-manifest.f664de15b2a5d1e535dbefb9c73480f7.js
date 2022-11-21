b5c44a42f2f18b6efd2e303feca0f4c291f4b9d8
mohammedalani1991 <mohammedalani1991@gmail.com>
2022-11-22 06:10:20 +0800

    Updates

---
+++ precache-manifest.f664de15b2a5d1e535dbefb9c73480f7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b2fc7fb3051c91c87aa75a556e2403",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/index.html"
+  },
+  {
+    "revision": "0cb96c3396db1c154075",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "e88062d3078cd9c1eece",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "fbf322967a7e0716537b",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/css/main.c4becf40.chunk.css"
+  },
+  {
+    "revision": "7033dfafdd95db21ba2e",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/js/2.8bf07e61.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/js/2.8bf07e61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb96c3396db1c154075",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/js/3.eb8b50f3.chunk.js"
+  },
+  {
+    "revision": "e88062d3078cd9c1eece",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/js/4.1c83c70d.chunk.js"
+  },
+  {
+    "revision": "fbf322967a7e0716537b",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/js/main.37de5db5.chunk.js"
+  },
+  {
+    "revision": "6d72f79efb32ed6e60a9",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/js/runtime-main.2e6ad60c.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "7bea9735a411331e63909de0121237a3",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/media/MosulUniversityLogo.7bea9735.png"
+  },
+  {
+    "revision": "81e8496f493ffab45cb15148c362fb73",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/media/almahfal.81e8496f.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "2465be52d1e4348efdfc8c24ff6589be",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/media/download.2465be52.jfif"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/precache-manifest.f664de15b2a5d1e535dbefb9c73480f7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/git@github.com:mohammedalani1991/mohammedalani1991.github.io.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
